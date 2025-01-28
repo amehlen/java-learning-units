a74b555f6f1da3579f8626fa264f891769e146a0
Allan Mehlen <mehlen.allan@gmail.com>
2025-01-28 15:31:26 +0100

    refactor(script): simplify PDF generation script

--- generate-pdf.sh
+++ generate-pdf.sh
@@ -1,40 +1,23 @@
 #!/bin/bash
 
-# Current folder (for docker volume)
+# Define paths
 current_folder="/$(pwd)"
-echo "Current folder: $current_folder"
-
-docs_folder="src/docs"
-echo "Docs folder: $docs_folder"
-
-asciidoc_file="$docs_folder/index.adoc"
-echo "Asciidoc file: $asciidoc_file"
-
-resources_folder="$docs_folder/resources"
-echo "Resources folder: $resources_folder"
-
-theme_folder="$resources_folder/themes/custom-theme.yml"
-echo "Theme file: $theme_folder"
-
-fonts_folder="$resources_folder/fonts"
-echo "Theme file: $fonts_folder"
-
+asciidoc_file="src/docs/index.adoc"
+theme_file="src/docs/resources/themes/custom-theme.yml"
+fonts_folder="src/docs/resources/fonts"
+output_folder="output"
+output_file="$output_folder/java-learning-units.pdf"
 docker_image="asciidoctor/docker-asciidoctor"
-echo "Docker image for asciidoctor: $docker_image"
 
 # Prepare output folder
-output_folder="output"
-echo "Output folder: $output_folder"
-
-echo "Preparing output folder..."
 rm -rf "$output_folder" && mkdir -p "$output_folder"
 
-output_file="$output_folder/java-learning-units.pdf"
-echo "Output file: $output_file"
-
-docker run --rm -v "$current_folder:/documents/" $docker_image asciidoctor-pdf \
+# Generate PDF
+docker run --rm -v "$current_folder:/documents/" "$docker_image" asciidoctor-pdf \
   -r asciidoctor-diagram \
   -r asciidoctor-mathematical \
-  -a pdf-theme="$theme_folder" \
+  -a pdf-theme="$theme_file" \
   -a pdf-fontsdir="$fonts_folder" "$asciidoc_file" \
   -o "$output_file"
+
+echo "PDF generated at: $output_file"
